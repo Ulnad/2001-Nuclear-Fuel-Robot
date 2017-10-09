ea5d751a815860e45ccf51347e1967ff2439c20f
nmliedtke <32607974+nmliedtke@users.noreply.github.com>
2017-10-08 20:37:31 -0400

    10-8-17

--- RBE2001Code/RBE2001Code.ino
+++ RBE2001Code/RBE2001Code.ino
@@ -1,9 +1,14 @@
 #include <Servo.h>
+#include <PID_v1.h>
+
 
 Servo rightmotor; // Servo object
 Servo leftmotor; // Servo object
 Servo arm;
 Servo gripper;
+
+bool turnedAround = false;
+bool grabbed = false;
 int n;
 int lineCounter = A0;
 int lineLeft = A1;
@@ -12,9 +17,21 @@ int lineBack = A3;
 int pot = A4;
 int  bumpSwitch = 29;
 int black = 700;
+int armBack = 26;
+int armDown = 385;
+int Kp = 1000;
+int Ki = 0;
+int Kd = 0;
+long desiredTime;
+double setpoint, inputValue, outputValue;
+PID pid(&inputValue, &outputValue, &setpoint, Kp, Ki, Kd, DIRECT);
 
 static enum runStates {decideNextState, determineBluetooth, navigateReactor, navigateDump, navigateNewRod, pickUp, putDown, returnRod, getNewRod, finished}
 runState;
+static enum turnAroundStates {backUp, skipLine, findLine}
+turnAroundState;
+static enum pickupStates {armDownRun, grip, armUp, turnAroundRun}
+pickupState;
 
 
 void setup() {
@@ -26,6 +43,11 @@ void setup() {
   gripper.attach(4, 1000, 2000);
   Serial.begin(9600);
   runState = navigateReactor;
+  pickupState = armDownRun;
+  turnAroundState = backUp;
+
+  pid.SetMode(AUTOMATIC);
+  pid.SetOutputLimits(-90, 90);
 
 }
 
@@ -51,7 +73,7 @@ void runRobot() {
 
       break;
     case pickUp:
-
+      pickupFunction();
       break;
     case putDown:
 
@@ -70,9 +92,8 @@ void runRobot() {
 }
 void loop() {
 
-
-runRobot();
-
+  runRobot();
+  Serial.println(analogRead(pot));
 
 }
 
@@ -92,6 +113,15 @@ void driveStraight() {
   leftmotor.write(180);
   rightmotor.write(0);
 }
+void driveTurnCenter() {
+
+  leftmotor.write(0);
+  rightmotor.write(180);
+}
+void driveBack() {
+  leftmotor.write(0);
+  rightmotor.write(180);
+}
 
 void driveLeft() {
   leftmotor.write(90);
@@ -106,5 +136,127 @@ void driveRight() {
 void stopMotors() {
   leftmotor.write(90);
   rightmotor.write(90);
+}
+
+void drive(double speed, Servo motor) {
+  if (speed > 90) speed = 90;
+  if (speed < -90) speed = -90;
+  motor.write(90 + speed);
+}
+void turnAround() {
+  switch (turnAroundState) {
+    case backUp:
+      if (millis < desiredTime) {
+        driveBack();
+      }
+      else {
+        stopMotors();
+        desiredTime = millis() + 300;
+        turnAroundState = skipLine;
+      }
+      break;
+    case skipLine:
+      if (millis < desiredTime) {
+        driveTurnCenter();
+      }
+      else {
+        stopMotors;
+        turnAroundState = findLine;
+      }
+      break;
+    case findLine:
+      if (analogRead(lineLeft) < black) {
+        driveTurnCenter();
+      }
+      else {
+        stopMotors();
+        turnAroundState = backUp;
+        turnedAround = true;
+      }
+      break;
+  }
+}
+void pickupFunction() {
+  switch (pickupState) {
+    case armDownRun:
+      if (analogRead(pot) < armDown) {
+        setpoint = armDown;
+        inputValue = analogRead(pot);
+        pid.Compute();
+        drive(-outputValue, arm);
+        gripper.write(0);
+      }
+      else {
+        stopMotors();
+        desiredTime = millis() + 1000;
+        arm.write(90);
+        pickupState = grip;
+      }
+      break;
+    case grip:
+      if (millis() < desiredTime) {
+        gripper.write(180);
+      }
+      else {
+        gripper.write(90);
+        pickupState = armUp;
+      }
+
+      break;
+    case armUp:
+      if (analogRead(pot) > armBack) {
+        setpoint = armBack;
+        inputValue = analogRead(pot);
+        pid.Compute();
+        drive(-outputValue, arm);
+      }
+      else {
+        stopMotors();
+        desiredTime = millis() + 300;
+pickupState = turnAroundRun;
+      }
+      break;
+    case turnAroundRun:
+if (!turnedAround) {
+         turnAround();
+       }
+       else{
+        runState = navigateDump;
+        pickupState = armDownRun;
+       }
+      break;
+  }
+
+  /* if ((analogRead(pot) < armDown) && (!grabbed)) {
+         setpoint = armDown;
+         inputValue = analogRead(pot);
+         pid.Compute();
+         drive(-outputValue, arm);
+         desiredTime = millis() + 1000;
+         gripper.write(0);
+       }
+       else if (millis() < desiredTime) {
+         arm.write(90);
+         gripper.write(180);
+         grabbed = true;
+       }
+       else if (analogRead(pot) > armBack) {
+         gripper.write(90);
+         setpoint = armBack;
+         inputValue = analogRead(pot);
+         pid.Compute();
+         drive(-outputValue, arm);
+         desiredTime = millis() + 500;
+
+       }
+       else if (!turnedAround) {
+         turnAround();
+       }
+
+       else {
+         runState = navigateDump;
+         turnedAround = false;
+       }
+  */
 }
 
